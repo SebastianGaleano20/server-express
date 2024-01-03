d6f4d9c74bf2db48aae35753e8abb35bb1e17ac3
Sebastian Galeano <sebascarp9@gmail.com>
2024-01-02 21:11:42 -0300

    actualizacion de raiz

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use("/", routes);
+app.use("/api", routes);
 
 app.listen(port, () => {
   console.log(`Servidor escuchando en http://localhost:${port}/`);
